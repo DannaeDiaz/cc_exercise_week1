18b2f27be8c2c4583a742feaa91ef531d2b78b5b
Alan Villanueva <alan.villanueva@itksquare.edu.mx>
2023-05-14 20:06:18 -0600

    solves

--- exercise.js
+++ exercise.js
@@ -20,7 +20,29 @@ func(['js','hola','false',2,1,false,'hola',2,'',true,'']) => output [ 'hola', 2,
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const getRepeated = (array) => {
   /* Only make changes below this comment */
+  if (Array.isArray(array)) { //Checking if the input is an array
 
+
+  const arrayLength = array.length
+  let repetitionArray = [] //variable to contain the elements with a repeated value
+  for ( let i = 0; i<arrayLength;i++){ //checking every element for a repeated value
+    for (let j=i+1; j<<arrayLength; j++){ //checking if the rest of the elements in the array are a repeated value
+      if (array[i]=== array[j]){ //checking if a pair of elements in the array are equal
+        repetitionArray.push(array[i]) //If equality holds, we add the element to the array of repeated elements
+        break
+      }
+    }
+    
+  }
+
+  repetitionArray = repetitionArray.filter((item, //we delete repeated values
+    index) => repetitionArray.indexOf(item) === index);
+  
+    return repetitionArray}
+
+  else{ return 'Please enter an array'}
+
+  
   /* Only make changes above this comment */
 };
 
